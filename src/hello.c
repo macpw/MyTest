3e5e4527e75c513ab136c3d8d3ac0f6fb8a8017c
Pawel Mac <pawel.mac@gmail.com>
2016-06-09 18:39:43 +0200

    add function 'helloWorld()' in hello.c

--- src/hello.c
+++ src/hello.c
@@ -1,8 +1,15 @@
 /* hello.c */
 #include <stdio.h>
 
+void helloWorld();
+
 int main()
 {
     printf( "Hello World\n" );
     return 0;
+}
+
+void helloWorld()
+{
+    printf( "Hello World!\n" );
 }
